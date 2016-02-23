a128feca6cc459cbb099d45ae929a04ec5fb41ec
Jason Han <jason.han@jpl.nasa.gov>
2016-02-23 13:20:35 -0800

    Formatting and add method to get all requirements

--- src/main/java/gov/nasa/jpl/mbee/doorsng/DoorsStatic.java
+++ src/main/java/gov/nasa/jpl/mbee/doorsng/DoorsStatic.java
@@ -1,7 +1,6 @@
 package gov.nasa.jpl.mbee.doorsng;
 
 import java.util.logging.Logger;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DoorsStatic {
@@ -9,42 +8,50 @@ public class DoorsStatic {
     private static final Logger logger = Logger.getLogger(DoorsStatic.class.getName());
 
     public static String createRequirement(String projectArea, Requirement requirement) throws Exception {
-	DoorsClient doors = new DoorsClient(projectArea);
-	ObjectMapper mapper = new ObjectMapper();
+        DoorsClient doors = new DoorsClient(projectArea);
+        ObjectMapper mapper = new ObjectMapper();
 
-	return mapper.writeValueAsString(doors.create(requirement));
+        return mapper.writeValueAsString(doors.create(requirement));
+    }
+
+    public static String readRequirement(String projectArea) throws Exception {
+        DoorsClient doors = new DoorsClient(projectArea);
+        ObjectMapper mapper = new ObjectMapper();
+
+        Requirement[] req = doors.getRequirements();
+
+        return mapper.writeValueAsString(req);
     }
 
     public static String readRequirement(String projectArea, String resourceUrl) throws Exception {
-	DoorsClient doors = new DoorsClient(projectArea);
-	ObjectMapper mapper = new ObjectMapper();
+        DoorsClient doors = new DoorsClient(projectArea);
+        ObjectMapper mapper = new ObjectMapper();
 
-	Requirement req = doors.getRequirement(resourceUrl);
+        Requirement req = doors.getRequirement(resourceUrl);
 
-	if (req.getTitle() == null) {
-	    RequirementCollection reqCol = doors.getRequirementCollection(resourceUrl);
-	    return mapper.writeValueAsString(reqCol);
-	}
+        if (req.getTitle() == null) {
+            RequirementCollection reqCol = doors.getRequirementCollection(resourceUrl);
+            return mapper.writeValueAsString(reqCol);
+        }
 
-	return mapper.writeValueAsString(req);
+        return mapper.writeValueAsString(req);
     }
 
-    public static String updateRequirement(String projectArea, String resourceUrl, Requirement requirement)
-	    throws Exception {
-	DoorsClient doors = new DoorsClient(projectArea);
-	ObjectMapper mapper = new ObjectMapper();
+    public static String updateRequirement(String projectArea, String resourceUrl, Requirement requirement) throws Exception {
+        DoorsClient doors = new DoorsClient(projectArea);
+        ObjectMapper mapper = new ObjectMapper();
 
-	Requirement req = doors.getRequirement(resourceUrl);
-	requirement.setResourceUrl(resourceUrl);
-	requirement.setEtag(req.getEtag());
+        Requirement req = doors.getRequirement(resourceUrl);
+        requirement.setResourceUrl(resourceUrl);
+        requirement.setEtag(req.getEtag());
 
-	return mapper.writeValueAsString(doors.update(requirement));
+        return mapper.writeValueAsString(doors.update(requirement));
     }
 
     public static String deleteRequirement(String projectArea, String resourceUrl) throws Exception {
-	DoorsClient doors = new DoorsClient(projectArea);
-	ObjectMapper mapper = new ObjectMapper();
+        DoorsClient doors = new DoorsClient(projectArea);
+        ObjectMapper mapper = new ObjectMapper();
 
-	return mapper.writeValueAsString(doors.delete(resourceUrl));
+        return mapper.writeValueAsString(doors.delete(resourceUrl));
     }
 }
