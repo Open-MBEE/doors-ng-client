cd041c5c41af59e19d417e9f840e925f3a6b3ae3
Jason Han <jason.han@jpl.nasa.gov>
2016-04-13 10:03:13 -0700

    Add dependencies and add back static class

---
+++ src/main/java/gov/nasa/jpl/mbee/doorsng/DoorsStatic.java
@@ -0,0 +1,52 @@
+package gov.nasa.jpl.mbee.doorsng;
+
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import gov.nasa.jpl.mbee.doorsng.model.Requirement;
+import gov.nasa.jpl.mbee.doorsng.model.RequirementCollection;
+import gov.nasa.jpl.mbee.doorsng.model.Folder;
+
+public class DoorsStatic {
+
+    private static final Logger logger = Logger.getLogger(DoorsStatic.class.getName());
+
+    public static DoorsClient initClient(String consumerKey, String consumerSecret, String user, String password, String webContextUrl, String projectArea) throws Exception {
+        return new DoorsClient(consumerKey, consumerSecret, user, password, webContextUrl, projectArea);
+    }
+
+    public static JSONObject createRequirement(DoorsClient doors, Requirement requirement) throws Exception {
+        return new JSONObject(doors.create(requirement));
+    }
+
+    public static JSONObject readRequirement(DoorsClient doors) throws Exception {
+        Requirement[] req = doors.getRequirements();
+
+        return new JSONObject(req);
+    }
+
+    public static JSONObject readRequirement(DoorsClient doors, String resourceUrl) throws Exception {
+        Requirement req = doors.getRequirement(resourceUrl);
+
+        if (req.getTitle() == null) {
+            RequirementCollection reqCol = doors.getRequirementCollection(resourceUrl);
+            return new JSONObject(reqCol);
+        }
+
+        return new JSONObject(req);
+    }
+
+    public static JSONObject updateRequirement(DoorsClient doors, String resourceUrl, Requirement requirement) throws Exception {
+        Requirement req = doors.getRequirement(resourceUrl);
+        requirement.setResourceUrl(resourceUrl);
+        requirement.setEtag(req.getEtag());
+
+        return new JSONObject(doors.update(requirement));
+    }
+
+    public static JSONObject deleteRequirement(DoorsClient doors, String resourceUrl) throws Exception {
+        return new JSONObject(doors.delete(resourceUrl));
+    }
+}
