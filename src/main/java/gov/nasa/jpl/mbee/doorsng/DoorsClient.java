820089ecbea6b13e1718390bfc70e9f30a54311a
BMJ <services@indefinite-domain.com>
2016-08-08 15:38:08 -0400

    compile error resolved, was missing import

--- src/main/java/gov/nasa/jpl/mbee/doorsng/DoorsClient.java
+++ src/main/java/gov/nasa/jpl/mbee/doorsng/DoorsClient.java
@@ -2,6 +2,7 @@ package gov.nasa.jpl.mbee.doorsng;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URL;
